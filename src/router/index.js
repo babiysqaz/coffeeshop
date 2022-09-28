c1567e16c219cffc4bd56b033aadbb3a867ba043
babiysqaz <babiysqaz@gmail.com>
2022-09-28 08:39:24 +0800

    fix: 路徑修改

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
+    {
+      path: "/coffeeshop/",
+      redirect: "/home",
+  },
   ]
 })
 
