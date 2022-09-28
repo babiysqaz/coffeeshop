4ee0bc07b1052bf96bf91ac10b35a211aa745dfb
babiysqaz <babiysqaz@gmail.com>
2022-09-28 08:52:18 +0800

    fix: 路徑設定修改

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/coffeeshop/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/coffeeshop/'
-    : '/'
 })
